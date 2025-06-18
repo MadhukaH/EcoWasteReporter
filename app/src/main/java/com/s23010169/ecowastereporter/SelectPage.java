7dac0763c1e0da0f32485aa1c3e65a2e84c2c214
Madhuka Hettiarachchi <138968964+MadhukaH@users.noreply.github.com>
2025-06-19 02:04:52 +0530

    LoginPage Create

--- app/src/main/java/com/s23010169/ecowastereporter/SelectPage.java
+++ app/src/main/java/com/s23010169/ecowastereporter/SelectPage.java
@@ -1,6 +1,7 @@
 package com.s23010169.ecowastereporter;
 
 import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -91,9 +92,7 @@ public class SelectPage extends AppCompatActivity {
             return;
         }
 
-        // TODO: Implement navigation to the next screen
-        // Intent intent = new Intent(SelectPage.this, NextActivity.class);
-        // intent.putExtra("SELECTED_ROLE", selectedRole);
-        // startActivity(intent);
+        Intent intent = new Intent(SelectPage.this, UserReg.class);
+        startActivity(intent);
     }
 } 
---
+++ app/src/main/java/com/s23010169/ecowastereporter/UserReg.java
@@ -0,0 +1,12 @@
+package com.s23010169.ecowastereporter;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserReg extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_reg);
+    }
+} 
