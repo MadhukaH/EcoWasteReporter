e45dd63ff2ce6e4fb98b5ac4b4ba15025d3e5603
Madhuka Hettiarachchi <138968964+MadhukaH@users.noreply.github.com>
2025-06-18 13:30:19 +0530

    Create Select Page

--- app/src/main/java/com/s23010169/ecowastereporter/MainActivity.java
+++ app/src/main/java/com/s23010169/ecowastereporter/MainActivity.java
@@ -1,6 +1,9 @@
 package com.s23010169.ecowastereporter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Set up navigation to SelectPage when "Let's Go" button is clicked
+        Button letsGoButton = findViewById(R.id.lets_go_button);
+        letsGoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectPage.class);
+                startActivity(intent);
+            }
         });
     }
 }
